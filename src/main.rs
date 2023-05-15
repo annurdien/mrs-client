fe4380ccc0689225375703c10352059770881a75
Annurdien Rasyid <annurdien@gmail.com>
2023-05-15 18:21:49 +0700

    refactor: use rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![feature(decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+pub mod control;
+pub mod system;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![index, cpu_usage])
+        .launch();
 }
